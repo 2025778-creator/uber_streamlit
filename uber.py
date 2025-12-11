d00c7465ba81d098013459ffca0161812ef1038e
2025778-creator <2025778@student.cct.ie>
2025-12-11 12:58:15 +0000

    Update uber.py

--- uber.py
+++ uber.py
@@ -8,22 +8,24 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 DATE_COLUMN = 'date/time'
- 
+
 def load_data(nrows):
-    data=pd.read_csv(DATA_URL, nrows=nrows)
+    data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN]=pd.to_datetime(data[DATE_COLUMN])
-    return
-    
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
 data_load_state = st.text('Loading data...')
 data = load_data(100000)
 data_load_state.text('Done!')
 
-if st.checkbox("Show Raw data"):
-    st.subheader("Row data")
+st.write(data)
+
+if st.checkbox('show raw data'):
+    st.subheader('Raw Data')
     st.write(data)
-    
+
 st.subheader('Number of pickups per hour')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
