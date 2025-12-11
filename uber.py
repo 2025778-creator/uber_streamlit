2fd51e0b0cc5f42e5b8cc0f7c6e4fa6cd4641cca
2025778-creator <2025778@student.cct.ie>
2025-12-11 12:18:45 +0000

    test

--- uber.py
+++ uber.py
@@ -3,3 +3,19 @@ import pandas as pd
 import numpy as np
 
 st.title("Uber pickups in New york city NYC")
+
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+            
+DATE_COLUMN = 'data/time'
+
+def load_data(nrows):
+    data=pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN]=pd.to_datatime(data[DATE_COLUMN])
+    return
+    
+data_load_state = st.text("Data Loading.....)
+data= load_data(10000)
+data_load_state.text("DONE")
